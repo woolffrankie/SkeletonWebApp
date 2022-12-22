fb29fbac35f4faa2400f09b7ee498e155031a386
Frankie Woolf <frankie@mcdanielwoolf.co.uk>
2022-12-22 12:46:58 +0000

    Hello to Hello there - test Git hub actions

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Hello!</h1>" +
+                "<h1>Hello there!</h1>" +
                 "<p>Please enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
