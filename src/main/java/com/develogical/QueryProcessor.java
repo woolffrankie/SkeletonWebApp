196621a1929f41d2fdab52f51c2544de9c53c977
Frankie Woolf <frankie@mcdanielwoolf.co.uk>
2022-12-22 14:43:57 +0000

    Knows our name.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -15,6 +15,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
 
+        if (query.toLowerCase().contains("your name")) {
+            return "AppleMac";
+        }
+
         return "";
     }
     
